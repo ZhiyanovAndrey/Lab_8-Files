367fefe2ddd1d30799361819fdf805bba2921784
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-26 16:35:26 +0300

    содержимое всех подкаталогов

--- CreateFile/Program.cs
+++ CreateFile/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace CreateFile
 {
+    /*2.Программно создайте текстовый файл и запишите в него 10 случайных чисел. 
+     * Затем программно откройте созданный файл, рассчитайте сумму чисел в нем,
+     * ответ выведите на консоль.*/
     class Program
     {
         static void Main(string[] args)
--- Lab_8 Files/Program.cs
+++ Lab_8 Files/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab_8_Files
 {
@@ -12,6 +13,30 @@ namespace Lab_8_Files
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\Андрей\source\repos";
+            Print(path,0);
+            Console.ReadKey();
+        }
+        static void Print(string path, int level) //второй аргумент метода устанавливает отступ
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+
+            if (dirs.Count() > 0 || files.Count() > 0)
+            {
+                string offset = new string('\t', level);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(offset + s);
+                }
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(offset + s);
+                    Print(s, level + 1); //Можно вызвать метод и внутри самого себя с путем к следующей папке
+                }
+            }
+
+
         }
     }
 }
