3a2c283f5b50cdb8dafd8a57490f580a36f871ea
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-26 07:10:32 +0300

    Добавьте файлы проекта.

---
+++ Lab_8 Files/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_Files
+{
+    //1.Выберите любую папку на своем компьютере, имеющую вложенные директории.
+    //Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
